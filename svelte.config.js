a27bdef34476fa7a27e6c159cbb9c5e8023af60f
Shyn <sinhdang@Shyns-Mac-mini.local>
2025-01-01 23:47:30 +0700

    update package

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const base = '/slick-portfolio-svelte-5';
+// const base = '/slick-portfolio-svelte-5';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,10 +13,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({ fallback: '404.html' }),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? base : ''
-		}
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+		// paths: {
+		// 	base: process.env.NODE_ENV === 'production' ? base : ''
+		// }
 	}
 };
 
